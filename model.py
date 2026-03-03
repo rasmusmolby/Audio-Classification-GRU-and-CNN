7e8b7390c842ff5d02c690fb9f418bb5c3da20f5
rasmusmolby <rallemolby@gmail.com>
2026-03-03 17:07:11 +0100

    Added fundamental structure in code

--- model.py
+++ model.py
@@ -1,27 +1,41 @@
 import torch
 import torch.nn as nn
 
+'''
+Paper dident specify activation functions in cnn block.
 
+Using MFCC with a 2:2 CNN and GRU.
+Sigmoid at end for binary crossentropy classification with softmax. shit balls
+
+Attention should be implemented in further iterations to increase accuracy (after every gru)
+
+
+
+
+
+'''
 class CNNGRU(nn.Module):
     def __init__(self, n_mfcc=39, c_cnn=64, n_classes = 3, gru_state=64):
         super().__init__()
-        self.cnn = nn.Sequential(
+        self.cnn1 = nn.Sequential(
             nn.Conv1d(n_mfcc, c_cnn, kernel_size=5, padding=2),
             nn.BatchNorm1d(c_cnn),
             nn.LeakyReLU(),
             nn.MaxPool1d(2),
             
         )
-        self.gru = nn.GRU(input_size=c_cnn, hidden_size=gru_state, batch_first=True) 
+        self.cnn2 = nn.Sequential(
+            nn.Conv1d(n_mfcc // 2, c_cnn, kernel_size=5, padding=2),
+            nn.BatchNorm1d(c_cnn),
+            nn.LeakyReLU(),
+            nn.MaxPool1d(2),
+            
+        )
+        self.gru1 = nn.GRU(input_size=c_cnn, hidden_size=gru_state, batch_first=True)
+        self.gru1 = nn.GRU(input_size=c_cnn, hidden_size=gru_state, batch_first=True) 
         self.fc = nn.Linear()
 
 
 
 
 
-def convloop1d(data, config):
-    nn.Conv1d(kernel_size=3, stride=1, in_channels=(39, 1), out_channels=(39, 32))
-
-
-
-    return null
