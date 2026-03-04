9cf09d866aab404ed225274a87358268e5f16cb7
rasmusmolby <rallemolby@gmail.com>
2026-03-04 08:35:18 +0100

    Changed structure of code

--- model.py
+++ model.py
@@ -10,30 +10,30 @@ Sigmoid at end for binary crossentropy classification with softmax. shit balls
 Attention should be implemented in further iterations to increase accuracy (after every gru)
 
 
-
+Remember to fix the transpose for the data extractor later on, as GRU wants a different tensor shape.
 
 
 '''
 class CNNGRU(nn.Module):
-    def __init__(self, n_mfcc=39, c_cnn=64, n_classes = 3, gru_state=64):
+    def __init__(self, n_mfcc=39, c_cnn=32, n_classes = 3, gru_state=64):
         super().__init__()
         self.cnn1 = nn.Sequential(
             nn.Conv1d(n_mfcc, c_cnn, kernel_size=5, padding=2),
             nn.BatchNorm1d(c_cnn),
             nn.LeakyReLU(),
-            nn.MaxPool1d(2),
+            nn.MaxPool1d(2, ceil_mode=True),
             
         )
         self.cnn2 = nn.Sequential(
-            nn.Conv1d(n_mfcc // 2, c_cnn, kernel_size=5, padding=2),
-            nn.BatchNorm1d(c_cnn),
+            nn.Conv1d(c_cnn, c_cnn * 2, kernel_size=5, padding=2),
+            nn.BatchNorm1d(c_cnn * 2),
             nn.LeakyReLU(),
-            nn.MaxPool1d(2),
+            nn.MaxPool1d(2, ceil_mode=True),
             
         )
         self.gru1 = nn.GRU(input_size=c_cnn, hidden_size=gru_state, batch_first=True)
-        self.gru1 = nn.GRU(input_size=c_cnn, hidden_size=gru_state, batch_first=True) 
-        self.fc = nn.Linear()
+        self.gru2 = nn.GRU(input_size=c_cnn, hidden_size=gru_state, batch_first=True) 
+        self.fc = nn.Linear(in_features= 123, out_features=n_classes)
 
 
 
